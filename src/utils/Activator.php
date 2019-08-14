6889cbcc93e72241c1d46b9db9140ab8eea0682e
Jhoe <jiggy@cru.io>
2019-08-14 09:23:51 +0800

    First commit: Issue still cannot download the data

---
+++ src/utils/Activator.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://cru.io
+ * @since      1.0.0
+ */
+
+namespace CruReports\utils;
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @author     CRU Team <info@cru.io>
+ */
+class Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+	
+	}
+}
