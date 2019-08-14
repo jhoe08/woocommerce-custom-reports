a5baf02e8894e961cf8ce86805967a993c4d01ed
Joegie Wagwag <red.mrjhon8@gmail.com>
2019-08-14 09:35:26 +0800

    Add files via upload

--- assets/admin/js/cru-reports.js
+++ assets/admin/js/cru-reports.js
@@ -1,123 +1,72 @@
 ( function( $ ){
-    'use strict';
-    
-    //console.log('Howdy!');
+	'use strict';
+	
+  // Init
+	var html = '';
+  var dateFrom = $('.data-crureports-from'), dateTo = $('.data-crureports-to'), dateBtn = $('.cru-reports-category-daterange');
+	
+  // Init Date
+  var date = new Date( Date.now() );
+  var getFirstDate = ( '01/01/' + date.getFullYear('Y') );
+  var getLastDate = ( '12/31/' + date.getFullYear('Y') );
 
-    var total_product_quantity = 0;
-    var total_product_amount = 0;
+  var dateFormat = "mm/dd/yy",
+  from = $( "#cru-reports-daterange-from" )
+    .attr( 'value', getFirstDate )
+    .datepicker({
+      //defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1
+    })
+    .on( "change", function() {
+      from.attr( 'value', getDateFormat( getDate( this ) ) );
+      to.datepicker( "option", "minDate", getDate( this ) );
+      dateFrom.text( setMonthDay( getDate( this ) ) );
+      dateBtn.attr('data-crureports-from', getDateFormat( getDate( this ) ) );
+    }),
+  to = $( "#cru-reports-daterange-to" )
+    .attr( 'value', getLastDate )
+    .datepicker({
+      //defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1
+    })
+    .on( "change", function() {
+      to.attr( 'value', getDateFormat( getDate( this ) ) );
+      from.datepicker( "option", "maxDate", getDate( this ) );
+      dateTo.text( setMonthDay( getDate( this ) ) );
+      dateBtn.attr('data-crureports-to', getDateFormat( getDate( this ) ) );
+  });
 
-    $( document ).on( 'click', 'a.cru-reports-category-tab', function( event ){
-    	event.preventDefault();
+  var daterange = $('.cru-reports-category-daterange'), daterangeForm = $('.cru-reports-category-daterange-form');
+  daterange.attr({
+    'data-crureports-from' : getFirstDate,
+    'data-crureports-to' : getLastDate
+  });
+  daterangeForm.hide().addClass('hide');
 
-    	var target = $('.cru-reports-products-info');
-    	var category_head = $('.cru-reports-category-info');
-    	var obj = $( this ).attr('data-json');
-    	var data = JSON.parse(obj);
-    	var html_head = '';
+  $( document ).on( 'click', 'a.cru-reports-category-daterange', function( event ){
+    return daterangeForm.hasClass('show') ? daterangeForm.hide().addClass('hide').removeClass('show') : daterangeForm.show().addClass('show').removeClass('hide');
+	});
 
-    	// reset content on the right side
-    	target.html('');
-    	category_head.html('');
-    	total_product_quantity = 0;
-    	total_product_amount = 0;
+  function setMonthDay( str ){
+    var str = String( str ), d = str.split(" "), md = d[1]+' '+d[2];
+    return md;
+  }
 
-    	var category_description = ( data.category_description !== '' ) ? data.category_description :  '<em>No category description</em>';
-    	
-    	html_head += '<div class="cru-reports-category-head">';
-    	html_head += '<h1>'+ data.category_name +'</h1>';
-    	html_head += '<h2>'+ category_description +'</h2>';    	
-    	html_head += '</div>';
+  function getDateFormat( date ){
+    var date = new Date( date ), day = date.getDate(), month = date.getMonth();
 
-    	var args = {
-    		action : 'cru_reports_get_product_by_category',
-    		category_id : data.category_id
-    	};
+    return (( month.toString().length == 1 ) ? ( ( month === 0 ) ? ( month.toString() + '1' ) : '0' + ( month + 1 ) ) : ( month + 1 )) + '/' + ( day.toString().length == 1 ? '0' + day : day ) + '/' + date.getFullYear();
+  }
 
-    	$.ajax({
-	        method: 'POST', // the method (could be GET btw)
-	        url: ajax_object.ajax_url, // The file where my php code is
-	        data: args,
-	        success : function( response ){ 
-	        	var products = JSON.parse( response );
-	        	var html = '';
-	        	var products_desc = '';
-	        	var products_sales = [];
-	        	var category_sales = 0;
-	        	var total_sales = 0;
-	        	
-	        	//console.log( response );
-
-	        	if( products.length > 0 ){
-
-
-		        	for (var i = 0; i < products.length; i++) {
-		        		//console.log( products[i].ID + '' + products[i].post_title );
-		        		products_desc = ( products[i].post_excerpt !== '' ) ? products[i].post_excerpt : '<em>No product descriptions</em>';
-
-		        		//var product = JSON.stringify(products[i], null, 4);  data-json="'+ product +'"
-		        		if ( 'sales' in products[i] ){
-		        			products_sales = cru_sales_info( Object.keys( products[i].sales ).map(function(key){
-			        			return [String(key), products[i].sales[key]];
-			        		}) );	
-		        		} else {
-		        			products_sales = '<p><strong>No Sales Information</strong></p>';
-		        		}
-
-		        		//console.log(products[i].sales);
-
-		        		html += '<div class="cru-reports-product-info">';
-		        		html += '<div class="cru-reports-product-head">';
-		        		html += '<h3>'+ products[i].post_title +'</h3>';
-		        		html += '<p>'+ products_desc +' ASDSA</p>';
-		        		html += '</div>';
-		        		html += '<div class="cru-reports-product-sales">'+ products_sales +'</div>';
-		        		html += '</div>';
-		        	}
-	        	} else {
-	        		html = '<p class="error">No products found on this category.</p>';
-	        	}
-
-	        	target.append( html );
-
-	        	total_sales = parseFloat(total_product_amount).toFixed(2);
-
-	        	html_head += '<div class="cru-reports-category-sales">';
-	        	html_head += '<p><label>Total Quantity:</label><span>'+total_product_quantity+'</span></p>';
-	        	html_head += '<p><label>Total Amount:</label><span>'+total_sales+'</span></p>';
-	        	html_head += '</div>';
-
-	        	category_head.append(html_head);
-	        },
-	        error : function( error ){ console.log( error ); }
-	    });
-    });
-
-    $( document ).on( 'click', 'div.download-reports > button', function( event ){
-
-    	var obj = $( this ).attr('data-json');
-    	var data = JSON.parse(obj);
-
-		var urlParams = new URLSearchParams( window.location.search );
-    	var getUrl = window.location.href;
-
-    	window.open( getUrl + '&download-csv' + '&categoryid=' + data.category_id + '&categoryname=' + data.category_name ,"_self");
-
-    });
-
-    function cru_sales_info( data ){
-    	var html = '';
-
-    	var quantity = Number(data[0][1]);
-    	var amount = Number(data[1][1]);
-
-
-    	html += '<p class="cru-reports quantity"><label>Quantity</label><span>'+ quantity +'</span></p>';
-		html += '<p class="cru-reports total"><label>Amount</label><span>'+ Number(amount).toFixed(2) +'</span></p>';
-
-		total_product_quantity += quantity;
-		total_product_amount += amount;
-
-    	return html;
+	function getDate( element ) {
+    var date;
+    try {
+      date = $.datepicker.parseDate( dateFormat, element.value );
+    } catch( error ) {
+      date = null;
     }
-
+    return date;   //Tue Jan 01 2019 00:00:00 GMT+0800 (Philippine Standard Time)
+  }
 })( jQuery );
